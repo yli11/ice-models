bb449c748a65dac61765e1b75dfa6e1efe624723
Johnny Gao <gaojy98@users.noreply.github.com>
2018-07-17 16:11:08 -0500

    tableau code for KT patterns

--- main.py
+++ main.py
@@ -144,20 +144,21 @@ def to_latex(count):
     return terms, latex
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description='Usage: python3 main.py <-i> <pattern>')
-    parser.add_argument('-i','--index', action='store_true',
-                        help="Replace uniform t with t_i's based on the row index of the vertex.")
-    parser.add_argument('input', nargs='+', type=int, help='Top row of the GT pattern.')
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(
+    #     description='Usage: python3 main.py <-i> <pattern>')
+    # parser.add_argument('-i','--index', action='store_true',
+    #                     help="Replace uniform t with t_i's based on the row index of the vertex.")
+    # parser.add_argument('input', nargs='+', type=int, help='Top row of the GT pattern.')
+    # args = parser.parse_args()
 
-    #text = input("Please enter the top row of the GT pattern:")
-    top_row = args.input #[int(x) for x in text.split()]
+    text = input("Please enter the top row of the GT pattern:")
+    #top_row = args.input #[int(x) for x in text.split()]
+    top_row = [int(x) for x in text.split()]
     GT = OrthogonalGTPatterns(top_row, True)
     summands = []
     to_be_weighted = []
     for gt in GT:
-        #print(gt)
+        print(gt)
         try:
             ice_model = Ice(gt)
             count = ice_model.fill_ice(gt, "alt")
@@ -172,7 +173,8 @@ if __name__ == "__main__":
             pass
     print('# of patterns: ' + str(len(list(GT))))
     #print('+ '.join(summands))
-    if args.index:
+    #if args.index:
+    if False:
         result = apply_weight_2(to_be_weighted, len(top_row) - 1)
     else:
         result = apply_weight(to_be_weighted, len(top_row) - 1)
--- tableau.py
+++ tableau.py
@@ -7,7 +7,7 @@ class Tableau:
     Author: Johnny Gao
     A tableau represents a tableaux with certain entries
     arg: a GT pattern
-    type: A or B. This determines the letters used. One can also determine its own letters
+    type: A or B or KT or Proctor. This determines the letters used. One can also determine its own letters
     shifted: if the tableau is shifted
     """
 
@@ -38,11 +38,22 @@ class Tableau:
                 self.dict[2 * i - 1] = str(i)
                 self.dict[2 * i] = self.overline(str(i))
             self.dict[self.size] = str(0)
+        elif type=='KT':
+            for i in range(1, int(self.size/3) + 1):
+                self.dict[3 * i - 2] = str(i)
+                self.dict[3 * i - 1] = self.overline(str(i))
+                self.dict[3 * i] = self.double_overline(str(i))
+        elif type=='Proctor':
+            for i in range(1, self.size + 1):
+                self.dict[i] = str(i)
         else:
             raise ValueError("Parameter 'type' has to be 'A' or 'B'")
 
     def overline(self, string):
         return ''.join(c+u'\u0305' for c in string)
+
+    def double_overline(self, string):
+        return ''.join(c+u'\u033F' for c in string)
 
     def __str__(self):
         string = ""
@@ -54,9 +65,9 @@ class Tableau:
 if __name__ == "__main__":
     text = input("Please enter the top row of the GT pattern:")
     top_row = [int(x) for x in text.split()]
-    GT = OrthogonalGTPatterns(top_row, True, 1)
+    GT = OrthogonalGTPatterns(top_row, False, 1)
     for gt in GT:
         print(gt)
-        tableaux = Tableau(gt, type='B', shifted=True)
+        tableaux = Tableau(gt, type='KT', shifted=False)
         print(tableaux)
     print(len(list(GT)))
